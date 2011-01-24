171b002469fcc15f3d0b60bfc5e2a886d1e56079
Mike Mehok <mehokm@duq.edu>
2011-01-24 19:41:08 +0000

    git-svn-id: https://www.evllabs.com/svn/jgaap/trunk@83 bcaee279-2e94-4b2e-be96-a1d7c6f3fc26

---
+++ src/com/jgaap/eventDrivers/FirstWordInSentenceEventDriver.java
@@ -0,0 +1,47 @@
+package com.jgaap.eventDrivers;
+
+import com.jgaap.generics.Document;
+import com.jgaap.generics.Event;
+import com.jgaap.generics.EventDriver;
+import com.jgaap.generics.EventSet;
+
+/**
+ * @author Mike Mehok
+ */
+public class FirstWordInSentenceEventDriver extends EventDriver {
+
+	@Override
+	public String displayName() {
+		return "First Word In Sentence";
+	}
+
+	@Override
+	public String tooltipText() {
+		return "First Word In Sentence";
+	}
+
+	@Override
+	public boolean showInGUI() {
+		return true;
+	}
+
+	@Override
+	public EventSet createEventSet(Document doc) {
+		EventSet es = new EventSet(doc.getAuthor());
+		//for (int i = 0; i < ds.documentCount(); i++) {
+			String current = doc.stringify();
+			String[] result = current.split("(((?<![A-Z])[?][\\s]*)|((?<![0-9])[?][\\s]*))|(((?<![A-Z])[!][\\s]*)|((?<![0-9])[!][\\s]*))|(((?<![A-Z])[a-z][.][\\s]++)|((?<=[0-9])[.][\\s]*))");
+        //regex splits at last letter of a word on a sentence ending with a period.
+        //EX: Im going to the mall. -- Im going to the mal
+			String splitSentence;
+			for (int j = 0; j < result.length; j++) {
+				splitSentence = result[j];
+				String[] wordsPerSent = splitSentence.split("[\\s++]|[,.][\\s++]");
+				es.addEvent(new Event(wordsPerSent[0]));
+			}
+		//}
+        return es;
+    }
+
+}
+
---
+++ unittests/com/jgaap/eventDrivers/SentenceLengthWithWordsEventDriverTest.java
@@ -0,0 +1,39 @@
+package com.jgaap.eventDrivers;
+
+/**
+ * @author Mike Mehok
+ */
+import static org.junit.Assert.*;
+
+import java.util.Vector;
+
+import org.junit.Test;
+
+import com.jgaap.generics.Document;
+import com.jgaap.generics.Event;
+import com.jgaap.generics.EventSet;
+
+public class SentenceLengthWithWordsEventDriverTest {
+
+	@Test
+	public void testCreateEventSetDocumentSet() {
+
+		Document doc = new Document();
+        doc.readStringText("Hello, Dr. Jones!  I'm not.feeling.too well today.  What's the matter Mr. Adams? My stomach hurts, or A.K.A, cramps.");
+
+		EventSet sampleEventSet = new SentenceLengthWithWordsEventDriver().createEventSet(doc);
+		EventSet expectedEventSet = new EventSet();
+		Vector<Event> tmp = new Vector<Event>();
+
+		tmp.add(new Event("3"));
+        tmp.add(new Event("4"));
+        tmp.add(new Event("5"));
+        tmp.add(new Event("6"));
+
+		expectedEventSet.addEvents(tmp);
+
+		assertTrue(expectedEventSet.equals(sampleEventSet));
+	}
+
+}
+
