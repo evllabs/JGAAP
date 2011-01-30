e56ad421cf1bce6a03605862176b0ce1010aedb4
Michael Ryan <mik3.ry4n@gmail.com>
2011-01-30 18:29:10 +0000

    Added javadoc to Util Added sanatizeShowInGUI method - this will take a list of Displayables and remove any elements that cannot be shown in the gui
    git-svn-id: https://www.evllabs.com/svn/jgaap/trunk@95 bcaee279-2e94-4b2e-be96-a1d7c6f3fc26

--- src/com/jgaap/backend/Utils.java
+++ src/com/jgaap/backend/Utils.java
@@ -5,11 +5,21 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
 
+import com.jgaap.generics.Displayable;
 import com.jgaap.generics.Document;
 
+/**
+ * Generic methods that will be reused throughout JGAAP
+ * 
+ * Any method added here is required to be static 
+ * When using Utils simply reference the method you want do not instance it.
+ * @author ryan
+ *
+ */
 public class Utils {
 
 	public static boolean saveFile(String filePath, String data){
@@ -52,5 +62,19 @@ public class Utils {
 			documents.add(document);
 		}
 		return documents;
+	}
+	/**
+	 * This method edits the list of Displayable objects you pass it so that it only contains ones with the showInGUI flag set to true
+	 * @param displayElements  List of Displayables to be edited 
+	 * @return Only showInGUI true Displayables
+	 */
+	public static List<Displayable> sanatizeShowInGUI(List<Displayable> displayElements){
+		Iterator<Displayable> iterator = displayElements.iterator();
+		while(iterator.hasNext()){
+			if(!iterator.next().showInGUI()){
+				iterator.remove();
+			}
+		}
+		return displayElements;
 	}
 }
