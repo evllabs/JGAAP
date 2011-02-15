8c5f0e86947c64108efefcced677fbe00d0c9cec
John Noecker Jr <jnoecker@gmail.com>
2011-02-15 19:40:44 +0000

    Added vector-based methods to distance functions.
    git-svn-id: https://www.evllabs.com/svn/jgaap/trunk@106 bcaee279-2e94-4b2e-be96-a1d7c6f3fc26

--- src/com/jgaap/generics/DistanceFunction.java
+++ src/com/jgaap/generics/DistanceFunction.java
@@ -17,6 +17,8 @@
  **/
 package com.jgaap.generics;
 
+import java.util.Vector;
+
 /**
  * This is an abstract class for distance functions. It is used, for example, in
  * variations of nearest-neighbor algorihms.
@@ -41,7 +43,37 @@ abstract public class DistanceFunction extends Parameterizable implements Compar
      * @return a double precision number (ideally a distance) btw es1 and es2
      */
     abstract public double distance(EventSet es1, EventSet es2);
-    
+
+    public double distance(Vector<Double> v1, Vector<Double> v2) {
+        EventSet es1 = new EventSet();
+        EventSet es2 = new EventSet();
+        int max = 0;
+
+        if(v1.size() > v2.size()) {
+            max = v1.size();
+        }
+        else {
+            max = v2.size();
+        }
+
+        for(Integer i = 0; i < max; i++) {
+            Event e = new Event(i.toString());
+            for(int j = 0; j < Math.round(v1.elementAt(i)); j++) {
+                es1.addEvent(e);
+            }
+        }
+
+        for(Integer i = 0; i < max; i++) {
+            Event e = new Event(i.toString());
+            for(int j = 0; j < Math.round(v2.elementAt(i)); j++) {
+                es2.addEvent(e);
+            }
+        }
+
+        return distance(es1, es2);
+
+    }
+
     public int compareTo(DistanceFunction o){
     	return displayName().compareTo(o.displayName());
     }
