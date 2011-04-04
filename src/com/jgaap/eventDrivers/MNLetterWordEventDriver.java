cf9a14aa77093f6c38b566b0778cf508b305565d
Patrick Brennan <brennan998@gmail.com>
2011-04-04 20:36:02 +0000

    Bug fix for MN based Event Drivers
    git-svn-id: https://www.evllabs.com/svn/jgaap/trunk@142 bcaee279-2e94-4b2e-be96-a1d7c6f3fc26

--- src/com/jgaap/eventDrivers/MNLetterWordEventDriver.java
+++ src/com/jgaap/eventDrivers/MNLetterWordEventDriver.java
@@ -45,7 +45,12 @@ public class MNLetterWordEventDriver extends EventDriver {
             MLabel.setText("M");
 
             MBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50" }));
-            MBox.setSelectedIndex(M-1);
+            String temp = this.getParameter("M");
+            if (temp.equals(""))
+            {
+                this.setParameter("M", 2);
+            }
+            MBox.setSelectedIndex(Integer.parseInt(this.getParameter("M")) - 1);
             MBox.addActionListener(new java.awt.event.ActionListener() {
                 public void actionPerformed(java.awt.event.ActionEvent evt) {
                     MBoxActionPerformed(evt);
@@ -56,7 +61,12 @@ public class MNLetterWordEventDriver extends EventDriver {
             NLabel.setText("N");
 
             NBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50" }));
-            NBox.setSelectedIndex(N-1);
+            temp = this.getParameter("N");
+            if (temp.equals(""))
+            {
+                this.setParameter("N", 3);
+            }
+            NBox.setSelectedIndex(Integer.parseInt(this.getParameter("N")) - 1);
             NBox.addActionListener(new java.awt.event.ActionListener() {
                 public void actionPerformed(java.awt.event.ActionEvent evt) {
                     NBoxActionPerformed(evt);
@@ -94,11 +104,11 @@ public class MNLetterWordEventDriver extends EventDriver {
         }
 
         private void NBoxActionPerformed(java.awt.event.ActionEvent evt) {
-            N = NBox.getSelectedIndex()+1;
+            this.setParameter("N", NBox.getSelectedIndex()+1);
         }
 
         private void MBoxActionPerformed(java.awt.event.ActionEvent evt) {
-            M = MBox.getSelectedIndex()+1;
+            this.setParameter("M", MBox.getSelectedIndex()+1);
         }
 
 	/** Underlying EventDriver from which Events are drawn. */
