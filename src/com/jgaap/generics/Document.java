870a3dfa4779bdb07cacfa02052ffbf03ba33d5e
Patrick Juola <juola@mathcs.duq.edu>
2011-04-04 16:40:04 +0000

    Stripped copyright notices.   Still passes 'ant build' and 'ant test' (-ish)
    git-svn-id: https://www.evllabs.com/svn/jgaap/trunk@138 bcaee279-2e94-4b2e-be96-a1d7c6f3fc26

--- src/com/jgaap/generics/Document.java
+++ src/com/jgaap/generics/Document.java
@@ -1,19 +1,4 @@
 /**
- *   JGAAP -- Java Graphical Authorship Attribution Program
- *   Copyright (C) 2009 Patrick Juola
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation under version 3 of the License.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  **/
 package com.jgaap.generics;
 
