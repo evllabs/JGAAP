d741637b6aa87f4ffea51a2fb79b9f938a84d2ad
John Noecker Jr <jnoecker@gmail.com>
2011-04-08 16:45:55 +0000

    Adapted K Nearest Neighbor driver to break ties using recursive calls to K-1 Nearest Neighbor.
    git-svn-id: https://www.evllabs.com/svn/jgaap/trunk@156 bcaee279-2e94-4b2e-be96-a1d7c6f3fc26

--- src/com/jgaap/classifiers/KNearestNeighborDriver.java
+++ src/com/jgaap/classifiers/KNearestNeighborDriver.java
@@ -4,6 +4,7 @@
 package com.jgaap.classifiers;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -20,7 +21,8 @@ import com.jgaap.generics.Pair;
  */
 public class KNearestNeighborDriver extends NeighborAnalysisDriver {
 
-    private final int DEFAULT_K = 3;
+    private final int DEFAULT_K = 5;
+    private final String DEFAULT_TIE = "lastPicked";
 
 	public String displayName() {
 		return "K-Nearest Neighbor Driver" + getDistanceName();
@@ -49,11 +51,18 @@ public class KNearestNeighborDriver extends NeighborAnalysisDriver {
             }
         }
 
+        String tieBreaker = DEFAULT_TIE; // Default
+        if(getParameter("tieBreaker") != null && !getParameter("tieBreaker").trim().equals("")) {
+            tieBreaker = getParameter("tieBreaker");
+        }
+
 		List<Pair<String, Double>> rawResults = new ArrayList<Pair<String,Double>>();
 
 		for (int i = 0; i < known.size(); i++) {
 			double current = distance.distance(unknown, known.get(i));
-			rawResults.add(new Pair<String, Double>(known.get(i).getAuthor(), current, 2));
+
+            rawResults.add(new Pair<String, Double>(known.get(i).getAuthor(), current, 2));
+
 			if (jgaapConstants.JGAAP_DEBUG_VERBOSITY) {
 				System.out.print(unknown.getDocumentName() + "(Unknown)");
 				System.out.print(":");
@@ -65,7 +74,11 @@ public class KNearestNeighborDriver extends NeighborAnalysisDriver {
 		Collections.sort(rawResults);
         for(int i = 0; i < Math.min(k, rawResults.size()); i++) {
             Pair<String, Double> p = rawResults.get(i);
-            ballot.vote(p.getFirst(), (1 + Math.pow(10, (-1.0 * (i+1)))));
+            ballot.vote(p.getFirst(), (1 + Math.pow(2, (-1.0 * (i+1)))));
+        }
+
+        if(tieBreaker.equals("lastPicked")) {
+            ballot.setComparator(new LastPickedComparator());
         }
 
         List<Pair<String, Double>> results = ballot.getResults();
@@ -73,4 +86,33 @@ public class KNearestNeighborDriver extends NeighborAnalysisDriver {
 		return results;
 	}
 
+    private class LastPickedComparator implements Comparator<Pair<String, Double>> {
+
+        public int compare(Pair<String, Double> firstPair, Pair<String, Double> secondPair) {
+            double first = firstPair.getSecond();
+            double second = secondPair.getSecond();
+
+            // If the overall rank was not the same, then return these according to rank.
+            if((int)first != (int)second) {
+                return (int)first - (int)second;
+            }
+
+            // Otherwise, we want to move the decimal point right until we have an integer.
+            while((int)first != first) {
+                first *= 2;
+                second *= 2;
+            }
+            // If first had fewer decimal places than second, this means the last first vote came BEFORE the last second vote.
+            if((int)second != second) {
+                return 1;
+            }
+            // Otherwise, the last second vote came before the last first vote.
+            else {
+                return -1;
+            }
+        }
+    }
+
 }
+
+
