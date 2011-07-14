0e236b8ab7fd5d4f38ecc138d596cb5986ac813c
Michael Ryan <mik3.ry4n@gmail.com>
2011-07-14 08:32:12 +0000

    Removing help text that was no longer helpful
    git-svn-id: https://www.evllabs.com/svn/jgaap/trunk@182 bcaee279-2e94-4b2e-be96-a1d7c6f3fc26

--- src/com/jgaap/backend/CLI.java
+++ src/com/jgaap/backend/CLI.java
@@ -39,29 +39,10 @@ public class CLI {
 						.println("\nWelcome to JGAAP"
 								+ "(Java Graphical Aurthorship Attribution Program) "
 								+ "\nCommandLine Interface: "
-								+ "\n\n\tjgaap [-c Canonicizer] [-lang Language] [-es EventSet] [-a AnalysisName] "
+								+ "\n\n\tjgaap [-c Canonicizer] [-lang Language] [-es EventSet] [-ec EventCuller] [-a AnalysisName] "
 								+ "\n\t      [-l CorpusCSV] [-mc] [-avg] [-rev] [-max] [-min] [-ad] [-s FileName]"
 								+ "\n\n\tTo view a list of valid arguments for any tag simply type jgaap help [tag]"
 								+ "\n\t\te.g. jgaap help a or jgaap help canonicizer"
-								+ "\n\n\tCanonicizers: [-c CANONICIZERNAME]"
-								+ "\n\t\tSmash Case: is used to noramlize case duh "
-								+ "\n\t\tNormalize Whitespace is used to normalize whitespace see a pattern "
-								+ "\n\t\tStrip HTML: will remove the HTML from a document so analysis can be "
-								+ "\n\t\tpreformed on what is written in that web document. "
-								+ "\n\t\tStrip Punctuation: will remove all punctuation from a document, "
-								+ "\n\t\tit will not replace it with anything."
-								+ "\n\t\tThe -c tag must be used before each canonicizor you wish to use and "
-								+ "\n\t\tthey are applied in the order they are typed."
-								+ "\n\n\tEvent Set: [-es EVENTSETNAME]"
-								+ "\n\t\tIn JGAAP you need to select an event set.  "
-								+ "\n\t\tIf you want this to only contain the 50 most common use the additional flag -mc "
-								+ "\n\t\tEvent Set Names: characters, character bigrams, character trigrams, "
-								+ "\n\t\tcharacter tetragrams, word lengths, syllables, Words, word bigrams, "
-								+ "\n\t\tword trigrams, word tetragrams."
-								+ "\n\n\tAnalysis: [-a ANALYSISNAME]"
-								+ "\n\t\tJGAAP currently has six methods "
-								+ "\n\t\tLDA,Linear SVM, Gaussian SVM, Histogram Distance, KolmogorovShmirnov distance, and  Cross Entropy "
-								+ "\n\t\t they can be called using the the names above on the command line."
 								+ "\n\n\tLoad Corpus: [-l CORPUSNAME]"
 								+ "\n\t\tThis feature takes the name of a csv file of the authors names and the "
 								+ "\n\t\tfiles paths of the associated documents and loads them into JGAAP."
@@ -71,7 +52,7 @@ public class CLI {
 								+ "\n\t\tThis feature will allow the use to add laguages to JGAAP by adding a "
 								+ "\n\t\tlanguage class to the com.jgaap.languages package."
 								+ "\n\t\tEnter a known language or one you have found/created there."
-								+ "\n\n\tFlags: [-mc, -avg, -rev, -max, -min, -ad]"
+								+ "\n\n\tFlags: [-avg, -rev, -max, -min]"
 								+ "\n\t\tMost Common [-mc]: only uses the 50 most common words accorss the documents."
 								+ "\n\t\tAverage Divergence [-avg]: the divergence in both directions averaged together."
 								+ "\n\t\tReverse Divergence [-rev]: the divergence is taken from the unknown document to the known."
@@ -142,58 +123,55 @@ public class CLI {
 			System.out.println(commandInput);
 			while (!commandInput.isEmpty()) {
 				Vector<String> currentTagSet = commandInput.remove(0);
-				while (!currentTagSet.isEmpty()) {
-					String currentArg = currentTagSet.remove(0);
-					if (currentArg.equalsIgnoreCase("-c")) {
-						String canonSelected = optionBuilder(currentTagSet);
-						canonicizers.add(canonSelected);
-					} else if (currentArg.equalsIgnoreCase("-es")) {
-						eventSelected = optionBuilder(currentTagSet);
-					} else if (currentArg.equalsIgnoreCase("-ec")){
-						eventCullersSelected.add(optionBuilder(currentTagSet));
-					}else if (currentArg.equalsIgnoreCase("-a")) {
-						analyzerSelected = optionBuilder(currentTagSet);
-					} else if (currentArg.equalsIgnoreCase("-d")) {
-						distanceSelected = optionBuilder(currentTagSet);
-					} else if (currentArg.equalsIgnoreCase("-avg")) {
-						currentDivergenceMethod = DivergenceType.Average;
-					} else if (currentArg.equalsIgnoreCase("-max")) {
-						currentDivergenceMethod = DivergenceType.Max;
-					} else if (currentArg.equalsIgnoreCase("-min")) {
-						currentDivergenceMethod = DivergenceType.Min;
-					} else if (currentArg.equalsIgnoreCase("-rev")) {
-						currentDivergenceMethod = DivergenceType.Reverse;
-					} else if (currentArg.equalsIgnoreCase("-cross")) {
-						currentDivergenceMethod = DivergenceType.Cross;
-					}else if (currentArg.equalsIgnoreCase("-l")) {
-						String csvFilePath = optionBuilder(currentTagSet);
-						documentMatrix = CSVIO.readCSV(csvFilePath);
-					} else if (currentArg.equalsIgnoreCase("-s")) {
-						saveResults = true;
-						saveFilePath = optionBuilder(currentTagSet);
-					} else if (currentArg.equalsIgnoreCase("-ee")) {
-						String loadEngineFilePath = optionBuilder(currentTagSet);
-						ExperimentEngine.runExperiment(loadEngineFilePath);
-						System.exit(0);
-					} else if (currentArg.equalsIgnoreCase("-lang")) {
-						language = optionBuilder(currentTagSet);
-					} else if (currentArg.equalsIgnoreCase("-ws")) {
-						String wSize = optionBuilder(currentTagSet);
-						System.out.println(wSize + " "
-								+ Integer.parseInt(wSize));
-						jgaapConstants.globalParams.setParameter("windowSize",
-								Integer.parseInt(wSize));
-					} else {
-						System.out.println("The following command"
-								+ (currentTagSet.size() > 1 ? "s" : "") + " "
-								+ (currentTagSet.size() > 1 ? "were" : "was")
-								+ " not recognized.");
-						System.out.println(currentArg);
-						for (int i = 0; i < currentTagSet.size(); i++) {
-							System.out.println(currentTagSet.remove(0));
-						}
-						System.exit(1);
+				String currentArg = currentTagSet.remove(0);
+				if (currentArg.equalsIgnoreCase("-c")) {
+					String canonSelected = optionBuilder(currentTagSet);
+					canonicizers.add(canonSelected);
+				} else if (currentArg.equalsIgnoreCase("-es")) {
+					eventSelected = optionBuilder(currentTagSet);
+				} else if (currentArg.equalsIgnoreCase("-ec")) {
+					eventCullersSelected.add(optionBuilder(currentTagSet));
+				} else if (currentArg.equalsIgnoreCase("-a")) {
+					analyzerSelected = optionBuilder(currentTagSet);
+				} else if (currentArg.equalsIgnoreCase("-d")) {
+					distanceSelected = optionBuilder(currentTagSet);
+				} else if (currentArg.equalsIgnoreCase("-avg")) {
+					currentDivergenceMethod = DivergenceType.Average;
+				} else if (currentArg.equalsIgnoreCase("-max")) {
+					currentDivergenceMethod = DivergenceType.Max;
+				} else if (currentArg.equalsIgnoreCase("-min")) {
+					currentDivergenceMethod = DivergenceType.Min;
+				} else if (currentArg.equalsIgnoreCase("-rev")) {
+					currentDivergenceMethod = DivergenceType.Reverse;
+				} else if (currentArg.equalsIgnoreCase("-cross")) {
+					currentDivergenceMethod = DivergenceType.Cross;
+				} else if (currentArg.equalsIgnoreCase("-l")) {
+					String csvFilePath = optionBuilder(currentTagSet);
+					documentMatrix = CSVIO.readCSV(csvFilePath);
+				} else if (currentArg.equalsIgnoreCase("-s")) {
+					saveResults = true;
+					saveFilePath = optionBuilder(currentTagSet);
+				} else if (currentArg.equalsIgnoreCase("-ee")) {
+					String loadEngineFilePath = optionBuilder(currentTagSet);
+					ExperimentEngine.runExperiment(loadEngineFilePath);
+					System.exit(0);
+				} else if (currentArg.equalsIgnoreCase("-lang")) {
+					language = optionBuilder(currentTagSet);
+				} else if (currentArg.equalsIgnoreCase("-ws")) {
+					String wSize = optionBuilder(currentTagSet);
+					System.out.println(wSize + " " + Integer.parseInt(wSize));
+					jgaapConstants.globalParams.setParameter("windowSize",
+							Integer.parseInt(wSize));
+				} else {
+					System.out.println("The following command"
+							+ (currentTagSet.size() > 1 ? "s" : "") + " "
+							+ (currentTagSet.size() > 1 ? "were" : "was")
+							+ " not recognized.");
+					System.out.println(currentArg);
+					for (int i = 0; i < currentTagSet.size(); i++) {
+						System.out.println(currentTagSet.remove(0));
 					}
+					System.exit(1);
 				}
 			}
 			try {
