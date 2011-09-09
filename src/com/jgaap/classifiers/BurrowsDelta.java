cdf363dc0c5d2e9996fb6c87b0d6da9f7a9b44a6
Michael Ryan <mik3.ry4n@gmail.com>
2011-09-09 15:01:02 -0400

    adding centroid capability to Burrows Delta

--- src/com/jgaap/classifiers/BurrowsDelta.java
+++ src/com/jgaap/classifiers/BurrowsDelta.java
@@ -2,8 +2,10 @@ package com.jgaap.classifiers;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import com.jgaap.backend.Utils;
@@ -31,53 +33,85 @@ public class BurrowsDelta extends AnalysisDriver {
 	}
 
 	/**
-	 * Burrows Delta using Argamon's Formula
-	 * Note this is sum(|(Xi - Yi) / sigma|)
-	 * (Basically, a Manhattan Distance normalized by the standard deviation
-	 * of a word across the known author list)
+	 * Burrows Delta using Argamon's Formula Note this is sum(|(Xi - Yi) /
+	 * sigma|) (Basically, a Manhattan Distance normalized by the standard
+	 * deviation of a word across the known author list)
 	 */
-    public List<Pair<String, Double>> analyze(EventSet unknown, List<EventSet> known) {
-    	List<Pair<String, Double>> results = new ArrayList<Pair<String, Double>>();
-    	Set<Event> allEvents = new HashSet<Event>();
-    	
-    	EventHistogram unknownHistogram = new EventHistogram();
-    	List<EventHistogram> knownHistograms = new ArrayList<EventHistogram>();
-    	
-    	for(Event e : unknown) {
-    		unknownHistogram.add(e);
-    		allEvents.add(e);
-    	}
-    	for(EventSet es : known) {
-    		EventHistogram currentKnownHistogram = new EventHistogram();
-    		knownHistograms.add(currentKnownHistogram);
-    		for(Event e : es) {
-    			currentKnownHistogram.add(e);
-    			allEvents.add(e);
-    		}
-    	}
-    	
-    	double[] delta = new double[known.size()];
-    	for(int i = 0; i < known.size(); i++) {
-    		delta[i] = 0.0;
-    	}
-    	for(Event event : allEvents) {
-    		List<Double> knownValues = new ArrayList<Double>();
-    		for(EventHistogram hist : knownHistograms) {
-    			knownValues.add(hist.getNormalizedFrequency(event));
-    		}
-    		double stddev = Utils.stddev(knownValues);
-    		for(int i = 0; i < delta.length; i++) {
-    			delta[i] = delta[i] + Math.abs((unknownHistogram.getNormalizedFrequency(event) - knownValues.get(i))/ stddev);
-    		}
-    	}
-    	
-    	for(int i = 0; i < known.size(); i++) {
-    		results.add(new Pair<String, Double>(known.get(i).getAuthor(), delta[i], 2));
-    	}
-    	
-    	Collections.sort(results);
-    	return results;
-    	
-    }
+	public List<Pair<String, Double>> analyze(EventSet unknown,
+			List<EventSet> known) {
+
+		boolean useCentroid = "true".equalsIgnoreCase(getParameter("centroid"));
+
+		List<Pair<String, Double>> results = new ArrayList<Pair<String, Double>>();
+		Set<Event> allEvents = new HashSet<Event>();
+
+		EventHistogram unknownHistogram = new EventHistogram();
+		List<EventHistogram> knownHistograms = new ArrayList<EventHistogram>();
+		
+		Map<String, List<EventHistogram>> authorHistogramMap = new HashMap<String, List<EventHistogram>>();
+
+		for (Event e : unknown) {
+			unknownHistogram.add(e);
+			allEvents.add(e);
+		}
+		for (EventSet es : known) {
+			EventHistogram currentKnownHistogram = new EventHistogram();
+			for (Event e : es) {
+				currentKnownHistogram.add(e);
+				allEvents.add(e);
+			}
+			if(useCentroid){
+				String author = es.getAuthor();
+				List<EventHistogram> value = authorHistogramMap.get(author);
+				if(value != null){
+					value.add(currentKnownHistogram);
+				} else {
+					List<EventHistogram> tmp = new ArrayList<EventHistogram>();
+					tmp.add(currentKnownHistogram);
+					authorHistogramMap.put(author, tmp);
+				}
+			} else {
+				knownHistograms.add(currentKnownHistogram);
+			}
+		}
+		
+		List<Map<Event, Double>> centroids = new ArrayList<Map<Event,Double>>();
+		
+		if(useCentroid){
+			for(List<EventHistogram> histograms : authorHistogramMap.values()){
+				centroids.add(Utils.makeCentroid(histograms));
+			}
+		}
+
+		double[] delta = new double[known.size()];
+		for (int i = 0; i < known.size(); i++) {
+			delta[i] = 0.0;
+		}
+		for (Event event : allEvents) {
+			List<Double> knownValues = new ArrayList<Double>();
+			if (useCentroid) {
+				for(Map<Event, Double> centroid : centroids){
+					knownValues.add(centroid.get(event));
+				}
+			} else {
+				for (EventHistogram hist : knownHistograms) {
+					knownValues.add(hist.getNormalizedFrequency(event));
+				}
+			}
+			double stddev = Utils.stddev(knownValues);
+			for (int i = 0; i < delta.length; i++) {
+				delta[i] = delta[i]
+						+ Math.abs((unknownHistogram.getNormalizedFrequency(event) - knownValues.get(i)) / stddev);
+			}
+		}
+
+		for (int i = 0; i < known.size(); i++) {
+			results.add(new Pair<String, Double>(known.get(i).getAuthor(), delta[i], 2));
+		}
+
+		Collections.sort(results);
+		return results;
+
+	}
 
 }
