69e26b4faef468b6afdca267ba374eb5e5a98d88
John Noecker Jr <jnoecker@gmail.com>
2011-09-10 17:27:06 -0400

    Added WEKA Naive Bayes class for testing WEKA adaptor.

---
+++ src/com/jgaap/classifiers/WEKANaiveBayes.java
@@ -0,0 +1,26 @@
+package com.jgaap.classifiers;
+
+import weka.classifiers.Classifier;
+
+public class WEKANaiveBayes extends WEKAAnalysis {
+
+	@Override
+	public String displayName() {
+		return "WEKA Naive Bayes Classifier";
+	}
+
+	@Override
+	public String tooltipText() {
+		return "Naive Bayes Classifier, Courtesy of WEKA";
+	}
+
+	@Override
+	public boolean showInGUI() {
+		return true;
+	}
+	
+	public WEKANaiveBayes() {
+		classifier = (Classifier)(new weka.classifiers.bayes.NaiveBayes());
+	}
+
+}
