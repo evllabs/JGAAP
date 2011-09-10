f0f068e52eb179db72b57883a32813e618caa036
John Noecker Jr <jnoecker@gmail.com>
2011-09-10 17:20:00 -0400

    Completed WEKA classifier adaptor class (yet untested)

--- src/com/jgaap/classifiers/WEKAAnalysis.java
+++ src/com/jgaap/classifiers/WEKAAnalysis.java
@@ -1,6 +1,7 @@
 package com.jgaap.classifiers;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -18,14 +19,15 @@ import com.jgaap.generics.EventSet;
 import com.jgaap.generics.Pair;
 
 /**
- * Generic WEKA classifier.  In theory, WEKA classifiers can extend this
- * and set the underlying classifier, plus any special parameters, in their
- * constructor, and everything else will Just Work.
+ * Generic WEKA classifier. In theory, WEKA classifiers can extend this and set
+ * the underlying classifier, plus any special parameters, in their constructor,
+ * and everything else will Just Work.
+ * 
  * @author John Noecker Jr.
- *
+ * 
  */
 public abstract class WEKAAnalysis extends AnalysisDriver {
-	
+
 	public Classifier classifier;
 
 	@Override
@@ -44,8 +46,8 @@ public abstract class WEKAAnalysis extends AnalysisDriver {
 	}
 
 	/**
-	 * Convert from the JGAAP event framework to the WEKA instance framework
-	 * to perform analysis.
+	 * Convert from the JGAAP event framework to the WEKA instance framework to
+	 * perform analysis.
 	 */
 	public List<List<Pair<String, Double>>> analyze(List<EventSet> unknownList,
 			List<EventSet> knownList) {
@@ -57,91 +59,128 @@ public abstract class WEKAAnalysis extends AnalysisDriver {
 		List<EventHistogram> knownHistograms = new ArrayList<EventHistogram>();
 		Set<String> allAuthorNames = new HashSet<String>();
 		Set<Event> allEvents = new HashSet<Event>();
-		for(EventSet eventSet : knownList) {
+		for (EventSet eventSet : knownList) {
 			allAuthorNames.add(eventSet.getAuthor());
 			EventHistogram currentKnownHistogram = new EventHistogram();
-			for(Event event : eventSet) {
+			for (Event event : eventSet) {
 				allEvents.add(event);
 				currentKnownHistogram.add(event);
 			}
 			knownHistograms.add(currentKnownHistogram);
 		}
-		
+
 		/*
 		 * Put together WEKA "Instances" object, which defines the attributes
 		 * (aka "events" or "features").
 		 */
 		FastVector attributeList = new FastVector(allEvents.size() + 1);
-		
+
 		FastVector authorNames = new FastVector(allAuthorNames.size());
-		for(String currentAuthorName : allAuthorNames) {
+		for (String currentAuthorName : allAuthorNames) {
 			authorNames.addElement(currentAuthorName);
 		}
 		Attribute authorNameAttribute = new Attribute("authorName", authorNames);
 		attributeList.addElement(authorNameAttribute);
-		
-		for(Event event : allEvents) {
+
+		for (Event event : allEvents) {
 			Attribute eventAttribute = new Attribute(event.getEvent());
-			attributeList.addElement(eventAttribute); // Each unique event is an attribute in WEKA
+			attributeList.addElement(eventAttribute); // Each unique event is an
+														// attribute in WEKA
 		}
-		
+
 		/*
 		 * Create the training "Instances" object, which is essentially the set
-		 * of feature vectors for the training data.  
+		 * of feature vectors for the training data.
 		 */
-		Instances trainingSet = new Instances("JGAAP", attributeList, knownList.size());
-		trainingSet.setClassIndex(0); // The label (author name) is in position 0.
-		
+		Instances trainingSet = new Instances("JGAAP", attributeList,
+				knownList.size());
+		trainingSet.setClassIndex(0); // The label (author name) is in position
+										// 0.
+
 		/*
 		 * Put together the training set
 		 */
-		for(int i = 0; i < knownHistograms.size(); i++) {
+		for (int i = 0; i < knownHistograms.size(); i++) {
 			EventHistogram knownHistogram = knownHistograms.get(i);
-			Instance currentTrainingDocument = new Instance(allEvents.size() + 1);
-			currentTrainingDocument.setValue((Attribute)attributeList.elementAt(0), knownList.get(i).getAuthor());
-			int j = 1; // Start counting events (at 1, since 0 is the author label)
-			for(Event event : allEvents) {
-				currentTrainingDocument.setValue((Attribute)attributeList.elementAt(j), knownHistogram.getNormalizedFrequency(event));
+			Instance currentTrainingDocument = new Instance(
+					allEvents.size() + 1);
+			currentTrainingDocument.setValue((Attribute) attributeList
+					.elementAt(0), knownList.get(i).getAuthor());
+			int j = 1; // Start counting events (at 1, since 0 is the author
+						// label)
+			for (Event event : allEvents) {
+				currentTrainingDocument.setValue(
+						(Attribute) attributeList.elementAt(j),
+						knownHistogram.getNormalizedFrequency(event));
 				j++;
 			}
 			trainingSet.add(currentTrainingDocument);
 		}
-		
+
 		/*
-		 * Train the classifier
-		 * N.B. The classifier should be set in the constructor by all subclasses.
+		 * Train the classifier N.B. The classifier should be set in the
+		 * constructor by all subclasses.
 		 */
 		try {
 			classifier.buildClassifier(trainingSet);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			List<Pair<String, Double>> errorResults = new ArrayList<Pair<String, Double>>();
-			Pair<String, Double> errorResult = new Pair<String, Double>("Error training classifier", 0.0);
+			Pair<String, Double> errorResult = new Pair<String, Double>(
+					"Error training classifier", 0.0);
 			errorResults.add(errorResult);
 			results.add(errorResults);
 			return results;
 		}
-		
+
 		/*
 		 * Generate the test sets, classifying each one as we go
 		 */
-		for(EventSet unknownEventSet : unknownList) {
+		for (EventSet unknownEventSet : unknownList) {
 			List<Pair<String, Double>> oneResult = new ArrayList<Pair<String, Double>>();
 			EventHistogram currentUnknownHistogram = new EventHistogram();
-			for(Event event : currentUnknownHistogram) {
+			for (Event event : currentUnknownHistogram) {
 				currentUnknownHistogram.add(event);
 			}
-			
+
 			Instance currentTest = new Instance(allEvents.size() + 1);
-			
-			currentTest.setValue((Attribute)attributeList.elementAt(0), "Unknown");
+
+			currentTest.setValue((Attribute) attributeList.elementAt(0),
+					"Unknown");
 			int i = 1; // Start at 1, again
-			for(Event event : allEvents) {
-				currentTest.setValue((Attribute)attributeList.elementAt(i), currentUnknownHistogram.getNormalizedFrequency(event));
+			for (Event event : allEvents) {
+				currentTest.setValue((Attribute) attributeList.elementAt(i),
+						currentUnknownHistogram.getNormalizedFrequency(event));
 			}
 			currentTest.setDataset(trainingSet);
+
+			double[] probDistribution;
+			try {
+				probDistribution = classifier
+						.distributionForInstance(currentTest);
+			} catch (Exception e) {
+				e.printStackTrace();
+				List<Pair<String, Double>> errorResults = new ArrayList<Pair<String, Double>>();
+				Pair<String, Double> errorResult = new Pair<String, Double>(
+						"Error training classifier", 0.0);
+				errorResults.add(errorResult);
+				results.add(errorResults);
+				return results;
+			}
+			
+			/*
+			 * Loop through the probability distributions (by author), and add results
+			 * as pairs (author, probability).
+			 */
+			int j = 0;
+			for(String authorName : allAuthorNames) {
+				oneResult.add(new Pair<String, Double>(authorName, probDistribution[j], 2));
+			}
+			Collections.sort(oneResult);
+			Collections.reverse(oneResult); // Reverse since we want higher probabilities first
+			results.add(oneResult);
 		}
-		
+
 		return results;
 	}
 
