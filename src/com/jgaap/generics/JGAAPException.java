058a02c2e0d68047a1a78a9cefb6e1f5cd051da1
Michael Ryan <mik3.ry4n@gmail.com>
2011-11-30 15:31:09 -0500

    fixing warning

--- src/com/jgaap/generics/JGAAPException.java
+++ src/com/jgaap/generics/JGAAPException.java
@@ -1,6 +1,8 @@
 package com.jgaap.generics;
 
 public class JGAAPException extends Exception {
+	private static final long serialVersionUID = 1L;
+
 	public JGAAPException(String message) {
 		super(message);
 	}
