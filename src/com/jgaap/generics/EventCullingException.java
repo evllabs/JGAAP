bccfcbc590cfcf9b9f8e0356c0de2abc58c6f39c
John Noecker Jr <jnoecker@gmail.com>
2011-11-30 11:15:14 -0500

    Added checks to FrequencyRangeCuller to ensure the range is acceptable. Fixes #67

--- src/com/jgaap/generics/EventCullingException.java
+++ src/com/jgaap/generics/EventCullingException.java
@@ -3,5 +3,9 @@ package com.jgaap.generics;
 public class EventCullingException extends Exception {
 
 	private static final long serialVersionUID = 1L;
+	
+	public EventCullingException(String message) {
+		super(message);
+	}
 
 }
