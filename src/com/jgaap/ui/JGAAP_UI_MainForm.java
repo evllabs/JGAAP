05d88937cbdba40b79bb1132ced2e0490dcff338
Michael Ryan <mik3.ry4n@gmail.com>
2012-06-28 08:16:26 -0400

    Fixing typo in the About dialog box It said JGAAP was released under the GPL v3.0 when JGAAP has really been released under the AGPL v3.0

--- src/com/jgaap/ui/JGAAP_UI_MainForm.java
+++ src/com/jgaap/ui/JGAAP_UI_MainForm.java
@@ -307,7 +307,7 @@ public class JGAAP_UI_MainForm extends javax.swing.JFrame {
 		jLabel11.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
 		jLabel11.setText("JGAAP 6.0");
 
-		jLabel12.setText("<html> JGAAP, the Java Graphical Authorship Attribution Program, <br/>is an opensource author attribution / text classification tool <br/>Developed by the EVL lab (Evaluating Variation in Language Labratory) <br/> Released by Patrick Juola under the GPL v3.0");
+		jLabel12.setText("<html> JGAAP, the Java Graphical Authorship Attribution Program, <br/>is an opensource author attribution / text classification tool <br/>Developed by the EVL lab (Evaluating Variation in Language Labratory) <br/> Released by Patrick Juola under the AGPL v3.0");
 
         jLabel13.setText("\u00A92012 EVL lab");
 
