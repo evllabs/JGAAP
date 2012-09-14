49b7a681d8fa14202637d9d65505d6867aeb062b
Michael Ryan <mik3.ry4n@gmail.com>
2012-09-14 12:22:47 -0400

    updating all methods to use addParams

--- src/com/jgaap/eventDrivers/CharacterNGramEventDriver.java
+++ src/com/jgaap/eventDrivers/CharacterNGramEventDriver.java
@@ -22,7 +22,6 @@ package com.jgaap.eventDrivers;
 import com.jgaap.generics.Document;
 import com.jgaap.generics.Event;
 import com.jgaap.generics.EventSet;
-import javax.swing.*;
 
 /**
  * Extract character N-grams as features.
@@ -30,12 +29,18 @@ import javax.swing.*;
  */
 public class CharacterNGramEventDriver extends NGramEventDriver {
 
-	JLabel NLabel = new JLabel();
-	JComboBox NBox = new JComboBox();
+	public CharacterNGramEventDriver() {
+		addParams("N", "N", "2", new String[] { "1", "2", "3", "4", "5", "6",
+				"7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17",
+				"18", "19", "20", "21", "22", "23", "24", "25", "26", "27",
+				"28", "29", "30", "31", "32", "33", "34", "35", "36", "37",
+				"38", "39", "40", "41", "42", "43", "44", "45", "46", "47",
+				"48", "49", "50" }, false);
+	}
 
 	@Override
 	public String displayName() {
-		return "Character " + getParameter("N") + "Grams";
+		return "Character NGrams";
 	}
 
 	@Override
@@ -54,68 +59,6 @@ public class CharacterNGramEventDriver extends NGramEventDriver {
 	}
 
 	@Override
-	public GroupLayout getGUILayout(JPanel panel) {
-
-		NLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		NLabel.setText("N");
-
-		NBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1",
-				"2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
-				"14", "15", "16", "17", "18", "19", "20", "21", "22", "23",
-				"24", "25", "26", "27", "28", "29", "30", "31", "32", "33",
-				"34", "35", "36", "37", "38", "39", "40", "41", "42", "43",
-				"44", "45", "46", "47", "48", "49", "50" }));
-		String temp = this.getParameter("N");
-		if (temp.equals("")) {
-			this.setParameter("N", 2);
-		}
-		NBox.setSelectedIndex(Integer.parseInt(this.getParameter("N")) - 1);
-		NBox.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				NBoxActionPerformed(evt);
-			}
-		});
-
-		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(panel);
-		layout.setHorizontalGroup(layout
-				.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-				.addGroup(
-						layout.createSequentialGroup()
-								.addContainerGap()
-								.addGroup(
-										layout.createParallelGroup(
-												javax.swing.GroupLayout.Alignment.LEADING)
-												.addComponent(NLabel)
-												.addComponent(
-														NBox,
-														javax.swing.GroupLayout.PREFERRED_SIZE,
-														javax.swing.GroupLayout.DEFAULT_SIZE,
-														javax.swing.GroupLayout.PREFERRED_SIZE))
-								.addContainerGap(275, Short.MAX_VALUE)));
-
-		layout.setVerticalGroup(layout
-				.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-				.addGroup(
-						layout.createSequentialGroup()
-								.addContainerGap()
-								.addComponent(NLabel)
-								.addPreferredGap(
-										javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-								.addComponent(NBox,
-										javax.swing.GroupLayout.PREFERRED_SIZE,
-										javax.swing.GroupLayout.DEFAULT_SIZE,
-										javax.swing.GroupLayout.PREFERRED_SIZE)
-								.addContainerGap(255, Short.MAX_VALUE)));
-		return layout;
-	}
-
-	private void NBoxActionPerformed(java.awt.event.ActionEvent evt) {
-		this.setParameter("N", NBox.getSelectedIndex() + 1);
-	}
-
-	// private NGramEventDriver theDriver;
-
-	@Override
 	public EventSet createEventSet(Document document) {
 		char[] text = document.getText();
 		int n;
@@ -129,17 +72,5 @@ public class CharacterNGramEventDriver extends NGramEventDriver {
 			eventSet.addEvent(new Event(new String(text, i, n)));
 		}
 		return eventSet;
-		// theDriver = new NGramEventDriver();
-		// String temp = this.getParameter("N");
-		// if (temp.equals(""))
-		// {
-		// this.setParameter("N", 2);
-		// }
-		// theDriver.setParameter("N", this.getParameter("N"));
-		// theDriver.setParameter("underlyingEvents", "Characters");
-		// theDriver.setParameter("opendelim", "null");
-		// theDriver.setParameter("closedelim", "null");
-		// theDriver.setParameter("separator", "null");
-		// return theDriver.createEventSet(ds);
 	}
 }
