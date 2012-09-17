8fddb458bb98f7b58af1a5bc3ddca8132ac63d5a
Michael Ryan <mik3.ry4n@gmail.com>
2012-09-17 03:27:00 -0400

    Updating what internal errors extend so they will be properly handled by the gui.

--- src/com/jgaap/generics/LanguageParsingException.java
+++ src/com/jgaap/generics/LanguageParsingException.java
@@ -1,6 +1,10 @@
 package com.jgaap.generics;
 
-public class LanguageParsingException extends Exception {
+public class LanguageParsingException extends JGAAPException {
+
+	public LanguageParsingException(String message) {
+		super(message);
+	}
 
 	private static final long serialVersionUID = 1L;
 
