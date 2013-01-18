c06459ec29306d4a743424a618904ba0e44522b3
Michael Ryan <mik3.ry4n@gmail.com>
2013-01-18 13:19:35 -0500

    Attempt to fix RBF network

--- src/com/jgaap/classifiers/WEKARBFNetwork.java
+++ src/com/jgaap/classifiers/WEKARBFNetwork.java
@@ -1,12 +1,15 @@
 package com.jgaap.classifiers;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.jgaap.generics.AnalyzeException;
 import com.jgaap.generics.Document;
 import com.jgaap.generics.WEKAAnalysisDriver;
 
 import weka.classifiers.Classifier;
+import weka.classifiers.functions.RBFNetwork;
 
 public class WEKARBFNetwork extends WEKAAnalysisDriver {
 
@@ -25,11 +28,20 @@ public class WEKARBFNetwork extends WEKAAnalysisDriver {
 		return true;
 	}
 	
+	private int numClusters = 1;
+	
 	public Classifier getClassifier() {
-		return (Classifier)(new weka.classifiers.functions.RBFNetwork());
+		RBFNetwork classifier = new RBFNetwork();
+		classifier.setNumClusters(numClusters);
+		return (Classifier)(classifier);
 	}
 
 	public void testRequirements(List<Document> knownList) throws AnalyzeException{
+		Set<String> authors = new HashSet<String>();
+		for(Document document : knownList) {
+			authors.add(document.getAuthor());
+		}
+		numClusters = authors.size();
 		//TODO: Need to figure out requirements using WEKARBFNetworkTest
 		return;
 	}
