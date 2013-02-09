7be1e756eaeeeaaea1ba656d6cd2e85827a99e24
Michael Ryan <mik3.ry4n@gmail.com>
2013-02-09 13:14:26 -0500

    Updating document loading to use apache tika Apache Tika is magic and lets us delete most libraries we had been using

--- src/com/jgaap/util/DocumentHelper.java
+++ src/com/jgaap/util/DocumentHelper.java
@@ -17,28 +17,12 @@
  */
 package com.jgaap.util;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.URL;
 
-import javax.swing.text.BadLocationException;
-import javax.swing.text.EditorKit;
-import javax.swing.text.html.HTMLDocument;
-import javax.swing.text.html.HTMLEditorKit;
-
 import org.apache.log4j.Logger;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.util.PDFTextStripper;
-import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.extractor.WordExtractor;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.tika.Tika;
 
 
 /**
@@ -52,35 +36,19 @@ import org.apache.poi.xwpf.usermodel.XWPFDocument;
 class DocumentHelper {
 	
 	static Logger logger = Logger.getLogger(com.jgaap.util.DocumentHelper.class);
-
+	static private Tika tika = new Tika();
+	
 	static char[] loadDocument(String filepath, String charset) throws Exception {
 		InputStream is;
-		int fileSize = -1;
-		char [] text;
 		if (filepath.startsWith("http://") || filepath.startsWith("https://")) {
 			URL url = new URL(filepath);
 			is = url.openStream();
 		} else if (filepath.startsWith("/com/jgaap/resources")){
 			is = com.jgaap.JGAAP.class.getResourceAsStream(filepath);
 		} else {
-			fileSize = (int) new File(filepath).length();
 			is = new FileInputStream(filepath);
 		} 
-		if (filepath.endsWith(".pdf")) {
-			text = loadPDF(is);
-		} else if (filepath.endsWith(".doc")) {
-			text = loadMSWord(is);
-		} else if (filepath.endsWith(".docx")){
-			text = loadMSWordDocx(is);
-		} else if (filepath.endsWith(".htm") || filepath.endsWith(".html")) {
-			text = loadHTML(is);
-		} else {
-			if(fileSize==-1)
-				text = readText(is, charset);
-			else
-				text = readText(is, charset, fileSize);
-		}
-		return text;
+		return tika.parseToString(is).toCharArray();
 	}
 
 	static Document.Type getDocType(String filepath) {
@@ -93,118 +61,6 @@ class DocumentHelper {
 		} else {
 			return Document.Type.GENERIC;
 		}
-	}
-
-	/**
-	 * Extracts text from a PDF and stores it in the document. Takes an input
-	 * stream rather than a file name.
-	 * 
-	 * @param filesInputStream
-	 *            An input stream pointing to a PDF file.
-	 * @throws IOException
-	 */
-	static private char[] loadPDF(InputStream filesInputStream)
-			throws IOException {
-		PDDocument doc = PDDocument.load(filesInputStream);
-		PDFTextStripper pdfStripper = new PDFTextStripper();
-		pdfStripper.setSortByPosition(false);
-		char[] origText = pdfStripper.getText(doc).toCharArray();
-		doc.close();
-
-		return origText;
-	}
-
-	/**
-	 * Extracts text from an HTML document and stores it in the document.
-	 * 
-	 * @param filesInputStream
-	 *            An input stream pointing to the HTML document to be read.
-	 * @throws BadLocationException
-	 * @throws IOException
-	 */
-	static private char[] loadHTML(InputStream filesInputStream)
-			throws IOException, BadLocationException {
-		EditorKit kit = new HTMLEditorKit();
-		HTMLDocument doc = (HTMLDocument) kit.createDefaultDocument();
-		doc.putProperty("IgnoreCharsetDirective", true);
-		kit.read(filesInputStream, doc, 0);
-		char[] origText = doc.getText(0, doc.getLength()).toCharArray();
-
-		return origText;
-	}
-
-	/**
-	 * Extracts text from a Word document and stores it in the document.
-	 * 
-	 * @param inputStream
-	 *            An input stream pointing to the Word document to be read.
-	 * @throws IOException
-	 */
-	static private char[] loadMSWord(InputStream inputStream)
-			throws IOException {
-		POIFSFileSystem fs = new POIFSFileSystem(inputStream);
-		HWPFDocument doc = new HWPFDocument(fs);
-		WordExtractor we = new WordExtractor(doc);
-		char[] origText = we.getText().toCharArray();
-
-		return origText;
-	}
-	
-	/**
-	 * Extracts text from a Word document and stores it in the document.
-	 * 
-	 * @param inputStream
-	 *            An input stream pointing to the Word document to be read.
-	 * @throws IOException
-	 */
-	static private char[] loadMSWordDocx(InputStream inputStream) throws IOException{
-		XWPFDocument docx = new XWPFDocument(inputStream);
-		XWPFWordExtractor extractor = new XWPFWordExtractor(docx);
-		return extractor.getText().toCharArray();
-	}
-	
-	/**
-	 * Reads text from a local file. The raw text of
-	 * the file is stored for quick access in an array.
-	 * 
-	 * @throws IOException
-	 **/
-	
-	static private char[] readText(InputStream is, String charset, int length) throws IOException{
-		Reader reader;
-		if(charset==null || charset.isEmpty()){
-			reader = new InputStreamReader(is);
-		} else {
-			reader = new InputStreamReader(is, charset);
-		}
-		char[] text = new char[length];
-		int status = reader.read(text);
-		if(status < length || reader.read() != -1)
-			logger.warn("Possibility Document too large to stor into memory or using utf-8 with utf-16 chars in it."); 
-		reader.close();
-		return text;
-	}
-	
-	/**
-	 * Reads text from a local file. The raw text of
-	 * the file is stored for quick access in an array.
-	 * 
-	 * @throws IOException
-	 **/
-	static private char[] readText(InputStream is, String charset) throws IOException {
-		int c;
-		StringBuilder stringBuilder = new StringBuilder();
-		BufferedReader reader;
-		if (charset==null||charset.isEmpty()) {
-			reader = new BufferedReader(new InputStreamReader(is));
-		} else {
-			reader = new BufferedReader(new InputStreamReader(is,charset));
-		}
-		while ((c = reader.read()) != -1) {
-			stringBuilder.append((char)c);
-		}
-		reader.close();
-		return stringBuilder.toString().toCharArray();
 	}
 
 }
