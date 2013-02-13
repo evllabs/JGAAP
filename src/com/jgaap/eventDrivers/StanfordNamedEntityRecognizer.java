953a4e754fd1b65f1f30a298f0fd159444094195
Michael Ryan <mik3.ry4n@gmail.com>
2013-02-13 17:33:51 -0500

    Removing unused ner models

--- src/com/jgaap/eventDrivers/StanfordNamedEntityRecognizer.java
+++ src/com/jgaap/eventDrivers/StanfordNamedEntityRecognizer.java
@@ -35,8 +35,7 @@ public class StanfordNamedEntityRecognizer extends EventDriver {
 	synchronized public EventSet createEventSet(char[] text)
 			throws EventGenerationException {
 		EventSet eventSet = new EventSet();
-		//		String serializedClassifier = "/com/jgaap/resources/models/ner/english.all.3class.distsim.crf.ser.gz";  original classifier
-		String serializedClassifier = "/com/jgaap/resources/models/ner/english.muc.7class.distsim.crf.ser.gz";  // Runs with this one too. Decide which to keep?!
+		String serializedClassifier = "/com/jgaap/resources/models/ner/english.muc.7class.distsim.crf.ser.gz";
 		if (classifier == null)
 			synchronized (this) {
 				if (classifier == null) {   
@@ -55,7 +54,6 @@ public class StanfordNamedEntityRecognizer extends EventDriver {
 			for (CoreLabel word : sentence) {
 				if (!word.get(AnswerAnnotation.class).equals("O")) {
 					eventSet.addEvent(new Event(word.word(), this));
-//					System.out.println(word.word()+"\t" + word.get(AnswerAnnotation.class));  // This can be removed.  Just shows each word being analyzed.
 				}
 			}		
 		}		
