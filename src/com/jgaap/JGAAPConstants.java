a1f7810a55b065a9babb1b4f18305899eb64f072
David Berdik <berdikd@duq.edu>
2019-04-14 17:38:35 -0400

    Changed version constant from "8.0.0-alpha-3" to "8.0.0-alpha-4"

--- src/com/jgaap/JGAAPConstants.java
+++ src/com/jgaap/JGAAPConstants.java
@@ -97,6 +97,6 @@ public class JGAAPConstants {
 	/**
 	 * Version Information
 	 */
-	public static final String VERSION = "8.0.0-alpha-3";
+	public static final String VERSION = "8.0.0-alpha-4";
 	public static final String YEAR = "2019";
 }
