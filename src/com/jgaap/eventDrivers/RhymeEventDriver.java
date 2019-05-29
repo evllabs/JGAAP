4d39f601773e58e851871369cba9d0de4cf2b48a
David Berdik <berdikd@duq.edu>
2019-05-29 14:21:44 -0400

    Modified RhymeEventDriver to get the last consonant cluster from the last word in each line

--- src/com/jgaap/eventDrivers/RhymeEventDriver.java
+++ src/com/jgaap/eventDrivers/RhymeEventDriver.java
@@ -1,5 +1,9 @@
 package com.jgaap.eventDrivers;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.jgaap.generics.EventDriver;
 import com.jgaap.generics.EventGenerationException;
 import com.jgaap.util.Event;
@@ -7,7 +11,7 @@ import com.jgaap.util.EventSet;
 
 /**
  * 
- * Gets the last character from the last word in each line.
+ * Gets the last consonant cluster from the last word in each line.
  * @author David Berdik
  *
  */
@@ -20,8 +24,7 @@ public class RhymeEventDriver extends EventDriver {
 
 	@Override
 	public String tooltipText() {
-		return "Gets the last character from the last word in each line. Punctuation is "
-				+ "included unless the \"Strip Punctuation\" canonicizer is used.";
+		return "Gets the last consonant cluster from the last word in each line.";
 	}
 
 	@Override
@@ -32,17 +35,27 @@ public class RhymeEventDriver extends EventDriver {
 	@Override
 	public EventSet createEventSet(char[] text) throws EventGenerationException {
 		EventSet rhymeEventSet = new EventSet();
+		Pattern consClusterPattern = Pattern.compile("(?i)[bcdfghjklmnpqrstvwxyz]{2,}");
 		
 		// Get lines from text.
 		EventSet linesEventSet = new NewLineEventDriver().createEventSet(text);
 		
-		// For each line, get the last word and extract the last character from it.
+		// For each line, get the last word and extract the last consonant cluster from it.
 		NaiveWordEventDriver wordDriver = new NaiveWordEventDriver();
 		for(Event line : linesEventSet) {
 			EventSet wordsEventSet = wordDriver.createEventSet(line.toString().toCharArray());
 			if (wordsEventSet.size() > 0) {
 				String lastWord = wordsEventSet.eventAt(wordsEventSet.size() - 1).toString();
-				rhymeEventSet.addEvent(new Event(lastWord.charAt(lastWord.length() - 1), this));
+				
+				// Get all consonant clusters from the last word.
+				Matcher consClusterMatcher = consClusterPattern.matcher(lastWord);
+				ArrayList<String> clusters = new ArrayList<>();
+				while(consClusterMatcher.find())
+					clusters.add(consClusterMatcher.group());
+				
+				// Get the last consonant cluster and add it to the EventSet.
+				if (clusters.size() > 0)
+					rhymeEventSet.addEvent(new Event(clusters.get(clusters.size() - 1), this));
 			}
 		}
 		
