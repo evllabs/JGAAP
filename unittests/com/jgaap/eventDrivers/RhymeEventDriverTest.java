3c5eecd977f51a125e61d55d552f8f5768fb0f9b
David Berdik <berdikd@duq.edu>
2019-05-30 09:51:58 -0400

    Updated "RhymeEventDriver" unit test to reflect changes made to event driver to detect clusters of length 1

--- unittests/com/jgaap/eventDrivers/RhymeEventDriverTest.java
+++ unittests/com/jgaap/eventDrivers/RhymeEventDriverTest.java
@@ -17,26 +17,34 @@ public class RhymeEventDriverTest {
 	public void testCreateEventDocumentSet() {
 		RhymeEventDriver eventDriver = new RhymeEventDriver();
 		
-		String sampleInput = "There once was a man from NantucKet\r\n" + 
-			"Who kept all his cash in a buDket.\r\n" + 
+		String sampleInput = "There once was a man from NantucKetDE\r\n" + 
+			"Who kept all his cash in a buDketDE.\r\n" + 
 			"	But his daughter, named Nan,\r\n" + 
 			"	Ran away with a man\r\n" + 
-			"And as for the bucket, Nantuffet.\r\n" + 
-			"But he followed the pair to Pawtutfet,\r\n" + 
+			"And as for the bucket, Nantuffer.\r\n" + 
+			"But he followed the pair to Pawtutfex,\r\n" + 
 			"The man and the girl with the bucket;\r\n" + 
 			"	And he said to the man,\r\n" + 
 			"	He was welcome to Nan,\r\n" + 
-			"But as for the bucket, Pawtufget.";
+			"But as for the bucket, PawtufgebcDFghjkLmNpQrStvWXz.";
 		EventSet actualSet = eventDriver.createEventSet(sampleInput.toCharArray());
 		
 		EventSet expectedSet = new EventSet();
-		expectedSet.addEvent(new Event("cK", eventDriver));
-		expectedSet.addEvent(new Event("Dk", eventDriver));
-		expectedSet.addEvent(new Event("ff", eventDriver));
-		expectedSet.addEvent(new Event("tf", eventDriver));
-		expectedSet.addEvent(new Event("ck", eventDriver));
-		expectedSet.addEvent(new Event("fg", eventDriver));
 		
+		for(int x = 0; x < 2; x++)
+			expectedSet.addEvent(new Event("tD", eventDriver));
+		
+		for(int x = 0; x < 2; x++)
+			expectedSet.addEvent(new Event("n", eventDriver));
+		
+		expectedSet.addEvent(new Event("r", eventDriver));
+		expectedSet.addEvent(new Event("x", eventDriver));
+		expectedSet.addEvent(new Event("t", eventDriver));
+		
+		for(int x = 0; x < 2; x++)
+			expectedSet.addEvent(new Event("n", eventDriver));
+		
+		expectedSet.addEvent(new Event("bcDFghjkLmNpQrStvWXz", eventDriver));
 		assertTrue(expectedSet.equals(actualSet));
 	}
 }
